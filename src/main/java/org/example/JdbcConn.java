8f2cefb2e8a68fc0307b2437d52dfb50a1183871
siddeshKG <kgsiddesh2517@gmail.com>
2024-09-17 12:58:19 +0530

    Created connection

---
+++ src/main/java/org/example/JdbcConn.java
@@ -0,0 +1,34 @@
+
+package org.example;
+import org.postgresql.Driver;
+
+import java.net.URL;
+import java.sql.*;
+
+public class JdbcConn {
+
+    /*
+    import package
+    load and register
+    create connection
+    Create statement
+    Execute statement
+    process results
+    close connection
+
+     */
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        String User ="postgres";
+        String pwd ="Siddu2525";
+        String url ="jdbc:postgresql://localhost:5433/JDBCdemo";
+
+        Class.forName("org.postgresql.Driver");
+        // driver has benn uploaded
+      Connection con= DriverManager.getConnection(url,User,pwd);
+      //connection is done
+        System.out.println("connection established");
+
+    }
+}
