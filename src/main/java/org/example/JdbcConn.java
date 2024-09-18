588d3bf9470c9ce0852035e72ac4f59e62836474
siddeshKG <kgsiddesh2517@gmail.com>
2024-09-18 19:48:05 +0530

    completing till execute querry

--- src/main/java/org/example/JdbcConn.java
+++ src/main/java/org/example/JdbcConn.java
@@ -23,12 +23,16 @@ public class JdbcConn {
         String User ="postgres";
         String pwd ="Siddu2525";
         String url ="jdbc:postgresql://localhost:5433/JDBCdemo";
-
+        String querry="";
         Class.forName("org.postgresql.Driver");
         // driver has benn uploaded
       Connection con= DriverManager.getConnection(url,User,pwd);
       //connection is done
         System.out.println("connection established");
+        //creating statement
+        Statement st = con.createStatement();
+        //executing queery
+        ResultSet rs=st.executeQuery(querry);
 
     }
 }
