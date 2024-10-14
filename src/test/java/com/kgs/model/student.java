c9223c858333c57e9349f31db75573e880d5cc85
siddeshKG <kgsiddesh2517@gmail.com>
2024-10-14 13:50:12 +0530

    added pojo classes

--- src/test/java/com/kgs/model/student.java
+++ src/test/java/com/kgs/model/student.java
@@ -1,4 +1,39 @@
 package com.kgs.model;
 
 public class student {
+
+    private int roll_num;
+    private String name;
+
+    public void setRoll_num(int roll_num) {
+        this.roll_num = roll_num;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRoll_num() {
+        return roll_num;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private String email;
+
+    public student(int roll_num, String name, String email) {
+        this.roll_num = roll_num;
+        this.name = name;
+        this.email = email;
+    }
 }
