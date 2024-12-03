ac5b91a0dc910159f3c31d06248ec710b1edb60e
siddeshKG <kgsiddesh2517@gmail.com>
2024-12-03 15:21:19 +0530

    staretd spring with xml file and scopes

--- src/main/java/org/example/Allien.java
+++ src/main/java/org/example/Allien.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Allien {
 
+   int age;
+    public Allien(){
+        System.out.println("object created.... ");
+    }
     public void code(){
         System.out.println("coding...");
     }
--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,14 @@ public class App
 
         Allien obi = (Allien) context.getBean("allien");
         obi.code();
+        obi.age =21;
+        System.out.println(obi.age);
+
+        Allien obi1 = (Allien) context.getBean("allien");
+        obi1.code();
+
+        System.out.println(obi1.age);
+
     }
 
 }
